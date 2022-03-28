dda9253e47780207a7d6fcf929b2729c7d186b05
Sahib Singh <75523820+ssahibsingh@users.noreply.github.com>
2022-03-28 16:48:32 +0530

    added meta tags

--- index.js
+++ index.js
@@ -16,40 +16,58 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
     var url = req.body.url;
-    const ID = url.slice(38, url.length);
-    var start, end;
-    if (req.body.start && req.body.end) {
-        if (req.body.start) {
-            start = req.body.start;
-        }
-        if (req.body.end) {
-            end = req.body.end;
-        }
-    }
-    else if (req.body.start) {
-        error = "Please enter 'To' Field";
+    if (url.length < 38 && url.slice(0, 38) != "https://www.youtube.com/playlist?list=") {
+        error = "Enter correct URL";
         res.render('index', { error: error, totalVideos: "", videoLen: "" });
-    }
-    else if (req.body.end) {
-        error = "Please enter 'From' Field";
-        res.render('index', { error: error, totalVideos: "", videoLen: "" });
-    }
 
-    (async () => {
-        let [totalV, Videos] = await getData(ID);
-        if (!(start || end)) {
-            start = 0;
-            end = Videos.length - 1;
-        }
-        else {
-            start = start - 1;
-            if (end == totalV) {
-                end = end - 1;
+    }
+    else {
+
+        const ID = url.slice(38, url.length);
+        // console.log(url);
+
+        var start, end;
+        if (req.body.start && req.body.end) {
+            if (req.body.start) {
+                start = req.body.start;
+            }
+            if (req.body.end) {
+                end = req.body.end;
             }
         }
-        let Time = await getTime(Videos, start, end);
-        res.render('index', { error: error, totalVideos: totalV, videoLen: Time });
-    })();
+        else if (req.body.start) {
+            error = "Please enter 'To' Field";
+            res.render('index', { error: error, totalVideos: "", videoLen: "" });
+        }
+        else if (req.body.end) {
+            error = "Please enter 'From' Field";
+            res.render('index', { error: error, totalVideos: "", videoLen: "" });
+        }
+
+        (async () => {
+            let [totalV, Videos] = await getData(ID);
+            if (!(start || end)) {
+                start = 0;
+                end = Videos.length - 1;
+            }
+            else {
+                start = start - 1;
+                if (end == totalV) {
+                    end = end - 1;
+                }
+            }
+            if(start<=end){
+                let Time = await getTime(Videos, start, end);
+                if (!Time) {
+                    res.render('index', { error: "Something went Wrong!!!", totalVideos: "", videoLen: "" });
+                }
+                res.render('index', { error: error, totalVideos: totalV, videoLen: Time });
+            }
+            else{
+                res.render('index', { error: "Incorrect Input for Range", totalVideos:"", videoLen: "" });
+            }
+        })();
+    }
 
 })
 
